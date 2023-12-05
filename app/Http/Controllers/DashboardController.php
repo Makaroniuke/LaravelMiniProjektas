52f88aa0a451cee28d2fdabeae422457dc53d407
Makaroniuke <laura.capaite123@gmail.com>
2023-12-05 21:53:35 +0200

    Laravel Mini Projektas

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\PostLiked;
+use Illuminate\Http\Request;
+use Mail;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+    public function index()
+    {
+        $user = auth()->user();
+        return view("dashboard");
+    }
+}
